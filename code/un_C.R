e3087f270755dbc7d9d6bf734e9ac7dc6c912478
Tommaso Rigon <tommaso.rigon@gmail.com>
2026-09-14 16:45:28 +0200

    asdasd

--- code/un_C.R
+++ code/un_C.R
@@ -3,110 +3,110 @@ getwd() # Identifies the working directory (get working directory)
 
 ## -----------------------------------------------------------------------------
 # THE PATH VARIABLE MUST BE CHANGED DEPENDING ON WHERE THE .csv FILE IS SAVED
-path <- "data/calcio.csv" # This is the path of the file, relative to getwd()
+path <- "data/football.csv" # This is the path of the file, relative to getwd()
 
 ## -----------------------------------------------------------------------------
 #| eval: false
-# path <- "https://tommasorigon.github.io/EnvEcoStat/data/calcio.csv"
+# path <- "https://tommasorigon.github.io/EnvEcoStat/data/football.csv"
 
 ## -----------------------------------------------------------------------------
-calcio <- read.table(path, header = TRUE, sep = ",")
+football <- read.table(path, header = TRUE, sep = ",")
 
 ## -----------------------------------------------------------------------------
 #| eval: false
-# calcio <- read.table("calcio.csv", header = TRUE, sep = ",")
+# football <- read.table("football.csv", header = TRUE, sep = ",")
 
 ## -----------------------------------------------------------------------------
-dim(calcio) # Equivalent to c(nrow(calcio), ncol(calcio))
+dim(football) # Equivalent to c(nrow(football), ncol(football))
 
 ## -----------------------------------------------------------------------------
-head(calcio) # Equivalent command: calcio[1:6, ]
+head(football) # Equivalent command: football[1:6, ]
 
 ## -----------------------------------------------------------------------------
-tail(calcio) # Equivalent command: calcio[1895:1900, ]
+tail(football) # Equivalent command: football[1895:1900, ]
 
 ## -----------------------------------------------------------------------------
-colnames(calcio) # To access the names of the variables
+colnames(football) # To access the names of the variables
 
 ## -----------------------------------------------------------------------------
-str(calcio)
+str(football)
 
 ## -----------------------------------------------------------------------------
-is.numeric(calcio$B365H) # Check that it is a variable of type numeric
-class(calcio$B365H)
-calcio$B365H[1:10] # First 10 elements of an R vector
+is.numeric(football$B365H) # Check that it is a variable of type numeric
+class(football$B365H)
+football$B365H[1:10] # First 10 elements of an R vector
 
 ## -----------------------------------------------------------------------------
-calcio$HomeTeam <- factor(calcio$HomeTeam)
-calcio$AwayTeam <- factor(calcio$AwayTeam)
-calcio$FTR <- factor(calcio$FTR)
+football$HomeTeam <- factor(football$HomeTeam)
+football$AwayTeam <- factor(football$AwayTeam)
+football$FTR <- factor(football$FTR)
 
 ## -----------------------------------------------------------------------------
-calcio <- read.table(path, header = TRUE, sep = ",", stringsAsFactors = TRUE)
+football <- read.table(path, header = TRUE, sep = ",", stringsAsFactors = TRUE)
 
 ## -----------------------------------------------------------------------------
-levels(calcio$HomeTeam)
+levels(football$HomeTeam)
 
 ## -----------------------------------------------------------------------------
-calcio$FTR[1:10]
-levels(calcio$FTR) <- c("Away", "Draw", "Home") # Rename the categories
-calcio$FTR[1:10]
+football$FTR[1:10]
+levels(football$FTR) <- c("Away", "Draw", "Home") # Rename the categories
+football$FTR[1:10]
 
 ## -----------------------------------------------------------------------------
-calcio$Draw <- calcio$FTR # Create a copy of the variable FTR called Draw
-levels(calcio$Draw) <- c("Not_Draw", "Draw", "Not_Draw") # Merging of categories
+football$Draw <- football$FTR # Create a copy of the variable FTR called Draw
+levels(football$Draw) <- c("Not_Draw", "Draw", "Not_Draw") # Merging of categories
 
-calcio$Draw[1:10]
-
-## -----------------------------------------------------------------------------
-calcio$Date <- as.Date(calcio$Date, format = "%Y-%m-%d")
-class(calcio$Date)
-calcio$Date[1:10]
+football$Draw[1:10]
 
 ## -----------------------------------------------------------------------------
-min(calcio$Date) # First match played
-max(calcio$Date) # Last match played
+football$Date <- as.Date(football$Date, format = "%Y-%m-%d")
+class(football$Date)
+football$Date[1:10]
 
 ## -----------------------------------------------------------------------------
-calcio[c(1806, 501, 109), ]
+min(football$Date) # First match played
+max(football$Date) # Last match played
 
 ## -----------------------------------------------------------------------------
-calcio_draw <- calcio[calcio$FTR == "Draw", ]
-head(calcio_draw)
+football[c(1806, 501, 109), ]
 
 ## -----------------------------------------------------------------------------
-calcio_home <- calcio[calcio$B365H > 9, ]
-calcio_home
+football_draw <- football[football$FTR == "Draw", ]
+head(football_draw)
 
 ## -----------------------------------------------------------------------------
-calcio[rowSums(is.na(calcio)) > 0, ] # Identifies the rows with missing values
+football_home <- football[football$B365H > 9, ]
+football_home
 
 ## -----------------------------------------------------------------------------
-calcio_no_na <- na.omit(calcio)
-dim(calcio_no_na)
+football[rowSums(is.na(football)) > 0, ] # Identifies the rows with missing values
 
 ## -----------------------------------------------------------------------------
-calcio_home <- subset(calcio, subset = B365H > 9)
-calcio_home
+football_no_na <- na.omit(football)
+dim(football_no_na)
 
 ## -----------------------------------------------------------------------------
-calcio_B365 <- subset(calcio, select = c(B365H, B365D, B365A))
-head(calcio_B365)
+football_home <- subset(football, subset = B365H > 9)
+football_home
 
 ## -----------------------------------------------------------------------------
-str(calcio)
+football_B365 <- subset(football, select = c(B365H, B365D, B365A))
+head(football_B365)
+
+## -----------------------------------------------------------------------------
+str(football)
 
 ## -----------------------------------------------------------------------------
 # Computation of the overround
-calcio$overround <- 1 / calcio$B365H + 1 / calcio$B365D + 1 / calcio$B365A - 1
+football$overround <- 1 / football$B365H + 1 / football$B365D + 1 / football$B365A - 1
 
 # Overround associated with Udinese-Parma of 1 September 2013
-subset(calcio, Date == "2013-09-01" & HomeTeam == "Udinese")
+subset(football, Date == "2013-09-01" & HomeTeam == "Udinese")
 
 # Minimum and maximum overround
-calcio[which.min(calcio$overround), ]
-calcio[which.max(calcio$overround), ]
+football[which.min(football$overround), ]
+football[which.max(football$overround), ]
 
 # The Serie A championship starts at the end of August and ends at the end of May
-calcio2009_2010 <- subset(calcio, Date >= "2009-08-15" & Date <= "2010-06-15")
-calcio2009_2010[which.max(calcio2009_2010$overround), ]
+football2009_2010 <- subset(football, Date >= "2009-08-15" & Date <= "2010-06-15")
+football2009_2010[which.max(football2009_2010$overround), ]
